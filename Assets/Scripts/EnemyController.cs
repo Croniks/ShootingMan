0dab0d26f6c37e574dfbe30214f94c755fd09fcb
Kroniks <wowmandarin@yandex.ru>
2021-04-14 18:10:40 +0300

    Added navigation grid and logic for player and enemy

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+[RequireComponent(typeof(NavMeshAgent))]
+[RequireComponent(typeof(Animator))]
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField] private List<Transform> _destinationPoints;
+    private int _currentDestinationIndex;
+
+    private Camera _mainCamera;
+    private NavMeshAgent _navMeshAgent;
+    private Animator _animator;
+    private Transform _selfTransform;
+
+
+    private void Awake()
+    {
+        _mainCamera = Camera.main;
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+        _animator = GetComponent<Animator>();
+        _selfTransform = GetComponent<Transform>();
+    }
+
+    private void Start()
+    {
+        if(_destinationPoints.Count > 0)
+        {
+            _selfTransform.position = _destinationPoints[0].position;
+            _currentDestinationIndex = 1;
+        }
+    }
+
+    private void Update()
+    {
+        if (!_navMeshAgent.pathPending && _navMeshAgent.remainingDistance < 0.5f)
+        {
+            GotoNextPoint();
+        }       
+    }
+
+    private void GotoNextPoint()
+    {
+        if (_destinationPoints.Count > 1)
+        {
+            _navMeshAgent.SetDestination(_destinationPoints[_currentDestinationIndex].position);
+            _currentDestinationIndex = (_currentDestinationIndex + 1) % _destinationPoints.Count;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+[RequireComponent(typeof(NavMeshAgent))] 
+[RequireComponent(typeof(Animator))]
+public class PlayerController : MonoBehaviour
+{
+    private Camera _mainCamera;
+    private NavMeshAgent _navMeshAgent;
+    private Animator _animator;
+    
+    
+    private void Awake()
+    {
+        _mainCamera = Camera.main;
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+        _animator = GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+        
+    }
+    
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            RaycastHit hit;
+            if(Physics.Raycast(_mainCamera.ScreenPointToRay(Input.mousePosition), out hit))
+            {
+                _navMeshAgent.SetDestination(hit.point);
+            }
+        }
+    }
+}
